a0705aaaa6af9d12fe75505fa50942dc91b60aa1
Itab Ghazouani <itab.ghazouani@gmail.com>
2025-02-14 00:07:40 +0100

    ts development issue fixed 6

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -8,11 +8,11 @@ import messageRoutes from './routes/message.route';
 import { checkCloudinaryConnection } from './lib/cloudinaryHealth';
 import { app, server } from './lib/socket';
 import path from 'path';
+import { fileURLToPath } from 'url';
 
 dotenv.config();
 
 const PORT = process.env.PORT;
-const __dirname = path.resolve();
 
 app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ extended: true, limit: '50mb' }));
@@ -27,12 +27,14 @@ app.use('/api/auth', authRoutes);
 app.use('/api/messages', messageRoutes);
 
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '/frontend/dist')));
+  const rootDir = path.resolve();
+  app.use(express.static(path.join(rootDir, '/frontend/dist')));
 
   app.get('*', (req, res) =>
-    res.sendFile(path.join(__dirname, '../frontend', 'dist', 'index.html')),
+    res.sendFile(path.join(rootDir, '/frontend/dist/index.html')),
   );
 }
+
 const startServer = async () => {
   try {
     const cloudinaryStatus = await checkCloudinaryConnection();
